2f9d6ffab7ee998f5448ba7de48f6a5d6d7bc0b4
Kevin Winter <kcgwinter@gmail.com>
2025-12-12 15:50:09 -0500

    add todofeature folder structure and placeholder classes

---
+++ Features/TodoList/Controllers/TodoController.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Features.TodoList.Controllers
+{
+    public class TodoController
+    {
+        
+    }
+}
