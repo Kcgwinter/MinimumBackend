9e82589be629ed35d0a648cba87bb0a2d2066646
Kevin Winter <kcgwinter@gmail.com>
2025-12-12 15:51:42 -0500

    add todolist dbcontext

--- Features/TodoList/Data/TodoDbContext.cs
+++ Features/TodoList/Data/TodoDbContext.cs
@@ -2,11 +2,24 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Features.TodoList.Models;
 
 namespace Features.TodoList.Data
 {
-    public class TodoDbContext
+    public class TodoDbContext(DbContextOptions<TodoDbContext> options)  : DbContext(options)
     {
-        
+        public DbSet<TodoItem> TodoItems => Set<TodoItem>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<TodoItem>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Title).IsRequired().HasMaxLength(200);
+                entity.Property(e => e.IsCompleted).IsRequired();
+            });
+        }
     }
 }
